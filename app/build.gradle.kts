82ee6bd56e5ea4a010768bd5c493d0b8c0939d65
faruk-fs12 <faruksav@gmail.com>
2026-09-14 13:40:44 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.nurisonay.freshwater"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     kotlinOptions { jvmTarget = "1.8" }
@@ -27,5 +27,6 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
